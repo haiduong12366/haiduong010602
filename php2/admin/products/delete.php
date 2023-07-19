cb86cceb792858d8c89383f534b14742f9a0115b
haiduong12366 <130330980+haiduong12366@users.noreply.github.com>
2023-07-19 13:58:41 +0700

    Delete php2 directory

--- php2/admin/products/delete.php
+++
@@ -1,9 +0,0 @@
-<?php   
-    $id = $_GET['id'];
-
-    include '../connect.php';
-    $sql = "delete from products 
-    where id = $id";
-
-    mysqli_query($connect,$sql);
-    mysqli_close($connect);
--- php2/footer.php
+++
@@ -1,3 +0,0 @@
-<div id="duoi">
-    Nhà cái 88, uy tín hàng đầu VNI
-</div>
